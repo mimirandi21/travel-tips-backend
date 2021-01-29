f57be6f7c119fb0c5220ff818cfe2f96c1645081
Randi Schreiner <mimirandi21@gmail.com>
2021-01-29 16:01:51 -0600

    updated seed file, default like/dislike to 0

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -1,7 +1,7 @@
 class TipsController < ApplicationController
 
     def create
-        tip = Tip.create(section_id: params[:section_id], place_id: params[:place_id], info: params[:info], color: params[:color])
+        tip = Tip.create(section_id: params[:section_id], place_id: params[:place_id], info: params[:info], color: params[:color], like_count=0, dislike_count=0)
         render json: tip
     end
 
