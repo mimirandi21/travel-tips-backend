a660e8a432bd412d28111fe3a89e3f937b594e67
Randi Schreiner <mimirandi21@gmail.com>
2021-01-30 18:14:43 -0600

    updated card backgrounds, add tips working

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -1,7 +1,7 @@
 class TipsController < ApplicationController
 
     def create
-        tip = Tip.create(section_id: params[:section_id], place_id: params[:place_id], info: params[:info], color: params[:color], like_count=0, dislike_count=0)
+        tip = Tip.create(tip_params(:section_id, :place_id, :info, :color, :like_count, :dislike_count))
         render json: tip
     end
 
@@ -20,7 +20,6 @@ class TipsController < ApplicationController
         tip = Tip.find(params[:id])
         render json: tip
     end
-end
 
     private
 
