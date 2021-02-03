678effa522d71181a4f8c25f019dd182e02eef90
Randi Schreiner <mimirandi21@gmail.com>
2021-02-03 13:49:04 -0600

    default page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :tips, only:[:create, :index, :show, :update]
   resources :sections, only: [:create, :index, :show]
   resources :places, only: [:create, :index, :show]
+  root 'application#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
