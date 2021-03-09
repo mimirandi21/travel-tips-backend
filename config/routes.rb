39d02b22f1dd56cfed62ed98033436421a2cedaa
mimirandi21 <mimirandi21@gmail.com>
2021-03-08 19:02:24 -0600

    updating routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   resources :tips
   resources :sections
   resources :places
-  root 'application#index'
-  get '/' to 'places#index'
-  get '/favicon.ico' to 'places#index'
+  
 end
